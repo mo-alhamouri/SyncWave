0e4f383d509c4c9fc3d8d286e58565c2663883f2
Mo Alhamouri <moh.alhamouri@gmail.com>
2026-06-14 07:55:42 +0400

    Updates

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Handle download button click
     const downloadBtns = document.querySelectorAll('.download-btn');
-    const RELEASE_VERSION = '1.2.0';
+    const RELEASE_VERSION = '1.2.1';
     const GITHUB_REPO = 'mo-alhamouri/SyncWave';
     
     const getDownloadUrl = async () => {
